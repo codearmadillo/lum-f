95fe23d3a67ce0c80fca5840bd5ec590b3cf8300
Jiří Královec <36158327+jiriKralovec@users.noreply.github.com>
2022-02-21 17:02:43 +0100

    Workspace setup

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include "lumiere/lib.h"
+
+#include "GL/glew.h"
+#include "GLFW/glfw3.h"
+
+#include "stb_image.h"
+
+extern "C" {
+    #include <lua.h>
+    #include <lualib.h>
+    #include <lauxlib.h>
+}
+
+int main() {
+    Foo a;
+    a.Bar();
+    return 0;
+}
---
+++ source/lib.cpp
@@ -0,0 +1,6 @@
+#include "lumiere/lib.h"
+#include <iostream>
+
+void Foo::Bar() {
+    std::cout << "Hello, World!" << std::endl;
+}
